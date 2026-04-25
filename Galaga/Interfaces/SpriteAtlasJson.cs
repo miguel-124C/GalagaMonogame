3e145785961182a05874fed316b2d898179e9c3f
miguel-124 <mikicuellar20@gmail.com>
2026-04-25 17:53:17 -0400

    Add SpriteAtlas and SpriteAtlasJson classes for sprite management

---
+++ Galaga/Interfaces/SpriteAtlasJson.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Galaga.Interfaces
+{
+
+    public partial class SpriteAtlasJson
+    {
+        [JsonPropertyName("texturePath")]
+        public string TexturePath { get; set; }
+
+        [JsonPropertyName("gap")]
+        public long Gap { get; set; }
+
+        [JsonPropertyName("sprites")]
+        public Sprites[] Sprites { get; set; }
+    }
+
+    public partial class Sprites
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("x")]
+        public long X { get; set; }
+
+        [JsonPropertyName("y")]
+        public long Y { get; set; }
+
+        [JsonPropertyName("w")]
+        public long W { get; set; }
+
+        [JsonPropertyName("h")]
+        public long H { get; set; }
+
+        [JsonPropertyName("totalFrames")]
+        public long TotalFrames { get; set; }
+    }
+}
---
+++ Galaga/Managers/SpriteAtlas.cs
@@ -0,0 +1,49 @@
+﻿using Galaga.Components;
+using Galaga.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Galaga.Managers
+{
+    public class SpriteAtlas(ContentManager cm, string pathJson)
+    {
+        private readonly ContentManager contentManager = cm;
+        private readonly string jsonPath = pathJson;
+
+        public Dictionary<string, Sprite> Sprites { get; private set; } = [];
+
+        public void LoadJson()
+        {
+            string json = File.ReadAllText(jsonPath);
+            var spriteAtlas = JsonSerializer.Deserialize<SpriteAtlasJson>(json);
+            
+            var texture = contentManager.Load<Texture2D>(spriteAtlas.TexturePath);
+
+            var sprites = spriteAtlas.Sprites;
+            foreach (var sprite in sprites)
+            {
+                Sprites[sprite.Name] = new Sprite
+                {
+                    Texture = texture,
+                    SourceRectangle = new Rectangle((int)sprite.X, (int)sprite.Y, (int)sprite.W, (int)sprite.H),
+                    TotalFrames = (int)sprite.TotalFrames,
+                    CurrentFrame = 0,
+                    SpriteEffect = SpriteEffects.None,
+                    TimeElapsed = 0f
+                };
+            }
+        }
+
+        public Sprite GetSprite(string name)
+        {
+            if (Sprites.TryGetValue(name, out Sprite value))
+                return value;
+
+            throw new KeyNotFoundException($"Sprite with name '{name}' not found in the atlas.");
+        }
+    }
+}
