cda7631208d8aaf4d4e3f80564bb4ae2e13e2c1b
miguel-124 <mikicuellar20@gmail.com>
2026-04-25 16:06:32 -0400

    Adding Tags components

---
+++ Galaga/Components/Tags.cs
@@ -0,0 +1,17 @@
+﻿using Galaga.Core.ECS;
+
+namespace Galaga.Components
+{
+
+    public struct Player          : IComponent { }
+    public struct Enemy           : IComponent { }
+    public struct PlayerBullet    : IComponent { }
+    public struct EnemyBullet     : IComponent { }
+    public struct Boos            : IComponent { }
+    public struct Bee             : IComponent { }
+    public struct Butterfly       : IComponent { }
+    public struct TractorBeam     : IComponent { }
+    public struct CapturedFighter : IComponent { }
+    public struct Explosion       : IComponent { }
+
+}
