c770ed64db903c736a807b72c67384e728fd2dc1
miguel-124 <mikicuellar20@gmail.com>
2026-04-29 23:36:51 -0400

    New component: SwarmData

---
+++ Galaga/Components/SwarmData.cs
@@ -0,0 +1,10 @@
+﻿using Galaga.Core.ECS;
+using Galaga.Interfaces;
+
+namespace Galaga.Components
+{
+    public struct SwarmData : IComponent
+    {
+        public EnemyDirection Direction;
+    }
+}
