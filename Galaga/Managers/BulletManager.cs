e1be620c6574d8eacb6b94794ef6412efc78ff04
miguel-124 <mikicuellar20@gmail.com>
2026-04-29 23:36:25 -0400

    Bullet and Systems Manager created

---
+++ Galaga/Managers/BulletManager.cs
@@ -0,0 +1,29 @@
+﻿using Galaga.Core.Events;
+using Galaga.Factories;
+using Microsoft.Xna.Framework;
+
+using Galaga.Interfaces;
+
+namespace Galaga.Managers
+{
+    internal class BulletManager
+    {
+        private readonly BulletFactory _bulletFactory;
+
+        public BulletManager(BulletFactory bulletFactory)
+        {
+            _bulletFactory = bulletFactory;
+            EventManager.OnPlayerShoot += HandlePlayerShoot;
+        }
+
+        private void HandlePlayerShoot(Vector2 position)
+        {
+            _bulletFactory.CreateBullet(position, BulletType.PlayerBullet);
+        }
+
+        private void HandleEnemyShoot(Vector2 position)
+        {
+            _bulletFactory.CreateBullet(position, BulletType.EnemyBullet);
+        }
+    }
+}
---
+++ Galaga/Managers/SystemsManager.cs
@@ -0,0 +1,24 @@
+﻿using Galaga.Core.ECS;
+using System.Collections.Generic;
+
+namespace Galaga.Managers
+{
+    public class SystemsManager
+    {
+        private readonly List<ISystem> systems = [];
+
+        public void RegisterSystem(ISystem system) => systems.Add(system);
+
+        public void UpdateSystems(float deltaTime)
+        {
+            foreach (var system in systems)
+                system.Update(deltaTime);
+        }
+
+        public void InitializeSystems(EntityManager entityManager)
+        {
+            foreach (var system in systems)
+                system.Initialize(entityManager);
+        }
+    }
+}
