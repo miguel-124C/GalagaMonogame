2acc995d884697fd97f21cc13e61cd75d5511686
miguel-124 <mikicuellar20@gmail.com>
2026-04-29 23:37:05 -0400

    Refactor GalagaGame class to enhance system management and initialization logic

--- Galaga/GalagaGame.cs
+++ Galaga/GalagaGame.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Galaga.Core.ECS;
+using Galaga.Factories;
+using Galaga.Managers;
+using Galaga.Systems;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,19 +10,38 @@ namespace Galaga
 {
     public class GalagaGame : Game
     {
-        private GraphicsDeviceManager _graphics;
+        private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private EntityManager _entityManager;
+        private SpriteAtlas _spriteAtlas;
+        private readonly SystemsManager _systemsManager;
+
+        private RenderSystem _renderSystem;
 
         public GalagaGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1024;   // Ancho
+            _graphics.PreferredBackBufferHeight = 768;   // Alto
+            _systemsManager = new SystemsManager();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _entityManager = new EntityManager();
+            _spriteAtlas = new SpriteAtlas(Content, "Content/galaga_atlas.json");
+
+            _systemsManager.RegisterSystem(new PlayerControlSystem(_graphics));
+            _systemsManager.RegisterSystem(new MovementSystem());
+            _systemsManager.RegisterSystem(new EnemyAISystem(_graphics));
+            _systemsManager.RegisterSystem(new CollisionSystem());
+
+            var bulletFactory = new BulletFactory(_entityManager, _spriteAtlas);
+            _ = new BulletManager(bulletFactory);
 
             base.Initialize();
         }
@@ -26,8 +49,17 @@ namespace Galaga
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _renderSystem = new RenderSystem(_spriteBatch);
+            _renderSystem.Initialize(_entityManager);
+            _systemsManager.InitializeSystems(_entityManager);
 
-            // TODO: use this.Content to load your game content here
+            _spriteAtlas.LoadJson();
+
+            EntityFactory ef = new(_entityManager, _spriteAtlas);
+            ef.CreatePlayer(new Vector2(400, 700));
+            EnemyFactory enemyFactory = new(_entityManager, _spriteAtlas);
+            enemyFactory.CreateButterfly(new Vector2(200, 100), []);
+            enemyFactory.CreateButterfly(new Vector2(250, 100), []);
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +67,18 @@ namespace Galaga
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _systemsManager.UpdateSystems(deltaTime);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _renderSystem.Update(deltaTime);
 
             base.Draw(gameTime);
         }
