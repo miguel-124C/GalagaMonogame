4b2da89898e34fd46f0ddf2025b2752be39b8d41
miguel-124 <mikicuellar20@gmail.com>
2026-04-30 20:57:22 -0400

    Implement swarm animations for enemy states in AnimationSystem

--- Galaga/Systems/AnimationSystem.cs
+++ Galaga/Systems/AnimationSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Galaga.Components;
 using Galaga.Core.ECS;
+using Galaga.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,6 +13,60 @@ namespace Galaga.Systems
             var explosions = EntityManager.GetEntitiesWith<Explosion>();
             if (explosions.Any())
                 ExplosionAnimation(deltaTime, explosions);
+
+            // TODO: EntityManager.GetEntitiesWith<SwarmData, EnemyStateData>();
+            var swarmData = EntityManager.GetEntitiesWith<EnemyStateData>();
+            if (swarmData.Any())
+            {
+                foreach (var entity in swarmData)
+                {
+                    var enemyStateData = EntityManager.GetComponent<EnemyStateData>(entity);
+
+                    switch (enemyStateData.State)
+                    {
+                        case EnemyState.Entering:
+                            SwarmEnteringAnimation(deltaTime, entity);
+                            break;
+                        case EnemyState.InFormation:
+                            SwarmInFormationAnimation(deltaTime, entity);
+                            break;
+                        case EnemyState.Diving:
+                            SwarmDivingAnimation(deltaTime, entity);
+                            break;
+                    };
+                }
+            }
+        }
+
+        private void SwarmEnteringAnimation(float deltaTime, uint entity)
+        {
+            var sprite = EntityManager.GetComponent<Sprite>(entity);
+            sprite.TimeElapsed += deltaTime;
+            if (sprite.TimeElapsed >= sprite.TimePerFrame){}
+        }
+
+        private void SwarmInFormationAnimation(float deltaTime, uint entity)
+        {
+            var sprite = EntityManager.GetComponent<Sprite>(entity);
+            sprite.TimeElapsed += deltaTime;
+            if (sprite.TimeElapsed >= sprite.TimePerFrame)
+            {
+                if (sprite.CurrentFrame == sprite.TotalFrames - 1)
+                    sprite.CurrentFrame = 0;
+                else
+                    sprite.CurrentFrame += 1;
+
+                sprite.TimeElapsed = 0;
+            }
+
+            EntityManager.AddComponent(entity, sprite);
+        }
+
+        private void SwarmDivingAnimation(float deltaTime, uint entity)
+        {
+            var sprite = EntityManager.GetComponent<Sprite>(entity);
+            sprite.TimeElapsed += deltaTime;
+            if (sprite.TimeElapsed >= sprite.TimePerFrame){}
         }
 
         private void ExplosionAnimation(float deltaTime, IEnumerable<uint> explosions)
